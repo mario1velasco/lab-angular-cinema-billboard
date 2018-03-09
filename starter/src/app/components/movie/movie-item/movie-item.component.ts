5197bd5ce678aff92b2969b06f1d616f52a0636d
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-09 13:34:50 +0100

    Feat: Service created just do log

--- starter/src/app/components/movie/movie-item/movie-item.component.ts
+++ starter/src/app/components/movie/movie-item/movie-item.component.ts
@@ -1,3 +1,4 @@
+import { MovieService } from './../../../shared/services/movie.service';
 import { Movie } from './../../../shared/models/movie.model';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -6,17 +7,21 @@ import { movies } from '../../../shared/data/sample-movies';
 @Component({
   selector: 'app-movie-item',
   templateUrl: './movie-item.component.html',
-  styleUrls: ['./movie-item.component.css']
+  styleUrls: ['./movie-item.component.css'],
+  providers: [
+    MovieService
+  ]
 })
 export class MovieItemComponent implements OnInit {
   movie:Movie;
   movies:Array<Movie>=movies;
 
   constructor(
-    private routes: ActivatedRoute
+    private routes: ActivatedRoute,
+    private movieService:MovieService
   ) {   }
 
-  ngOnInit() {
+  ngOnInit():void {
     this.routes.params.subscribe(params => {
       this.movie = this.movies.find(movie => movie.id === Number(params['id']));
     });
@@ -24,4 +29,7 @@ export class MovieItemComponent implements OnInit {
     // return this.movies.find(contact => contact.id === id);
   }
 
+  incrment():void{
+    this.movieService.inc();
+  }
 }
