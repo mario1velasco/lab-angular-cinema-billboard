a1776ba24bf22bfb854c03540524bb081ff262ac
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-09 10:13:34 +0100

    Feat: Movie Model creted

---
+++ starter/src/app/shared/models/movie.model.ts
@@ -0,0 +1,12 @@
+export class Movie {
+  id:string;
+title: string;
+poster : string;
+synopsis :string;
+genres :Array<String>;
+year: Number;
+director: string;
+actors: Array<String>;
+hours: Array<String>;
+room :number;
+}
