2fd121ea40b5763a85bf409c5d692d9f827ed069
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-09 11:37:53 +0100

    Feat: in Item paint all movies

--- starter/src/app/components/movie/movie-item/movie-item.component.ts
+++ starter/src/app/components/movie/movie-item/movie-item.component.ts
@@ -1,5 +1,7 @@
+import { Movie } from './../../../shared/models/movie.model';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { movies } from '../../../shared/data/sample-movies';
 
 @Component({
   selector: 'app-movie-item',
@@ -7,12 +9,16 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./movie-item.component.css']
 })
 export class MovieItemComponent implements OnInit {
+  movie:Movie;
+  movies:Array<Movie>=movies;
 
   constructor(
     private routes: ActivatedRoute
   ) {   }
 
   ngOnInit() {
+    // return this.movies;
+    // return this.movies.find(contact => contact.id === id);
   }
 
 }
