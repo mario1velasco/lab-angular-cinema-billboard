a694dd30a50d1df7cfb9d11376a5fe4671c1b337
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-09 19:24:49 +0100

    Feat: All done

--- starter/src/app/app.module.ts
+++ starter/src/app/app.module.ts
@@ -8,13 +8,17 @@ import { HomeComponent } from './components/misc/home/home.component';
 import { MovieComponent } from './components/misc/movie/movie.component';
 import { routes } from './app.routes';
 import { MovieItemComponent } from './components/movie/movie-item/movie-item.component';
+import { NavbarComponent } from './components/misc/navbar/navbar.component';
+import { FooterComponent } from './components/misc/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MovieComponent,
-    MovieItemComponent
+    MovieItemComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
